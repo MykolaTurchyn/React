5b87fe876d3d4fe9e58fe9da36a8744a4f8e189d
MykolaTurchn14 <tyrchnmike@gmail.com>
2021-07-12 17:42:59 +0300

    qqq

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -4,33 +4,39 @@ import {useState} from "react";
 export default function Counter() {
     const counter = useSelector((
         {counter: {value}}) => value);
-    let [numbers, setNumbers] = useState({number: ''});
-    const dispatch = useDispatch()
+    let [numbers, setNumbers] = useState({});
 
+    let [xxx, setXxx] = useState(0);
+
+    const dispatch = useDispatch()
+    const zzz = () => setXxx(numbers.value)
 
     function onSubmit(e) {
-        console.log(e.target.number)
         e.preventDefault();
-        console.log(numbers)
     }
 
-    function onNumberChange(e) {
-        setNumbers(e.target)
-}
-return (
-    <div>
-        <h2>{counter}</h2>
-        <button onClick={() => dispatch({type: 'INC'})}>INCRYMENT</button>
-        <br/><br/>
-        <button onClick={() => dispatch({type: 'DEC'})}>DECRYMENT</button>
-        <br/><br/>
-        <button onClick={() => dispatch({type: 'RESET'})}>RESET</button>
-        <br/><br/>
 
-        <form onSubmit={onSubmit}>
-            <input type="number" name={'number'} value={numbers} onChange={onNumberChange}/><br/>
-            <button>Custom INC</button>
-        </form>
-    </div>
-);
+
+    const onNumberChange = ({target: {value}}) => setNumbers({...numbers, value})
+
+    console.log(numbers, 'numbers')
+    console.log(xxx, 'xxx')
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => dispatch({type: 'INC'})}>INCRYMENT</button>
+            <br/><br/>
+            <button onClick={() => dispatch({type: 'DEC'})}>DECRYMENT</button>
+            <br/><br/>
+            <button onClick={() => dispatch({type: 'RESET'})}>RESET</button>
+            <br/><br/>
+
+            <h2>{+xxx}</h2>
+
+            <form onSubmit={onSubmit}>
+                <input type="number" name={'number'} value={numbers.value} onChange={onNumberChange}/><br/>
+                <button onClick={zzz}>Custom</button>
+            </form>
+        </div>
+    );
 }

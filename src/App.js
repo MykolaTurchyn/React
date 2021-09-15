8f88e910bfe323f7a6ec0e3846bedf0d43b5cb21
mykolaturchyn <tyrchnmikigt@gmail.com>
2021-09-15 13:44:42 +0300

    error with send car

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-export default function App () {
-    return(
-           <div>
+import {useEffect, useState} from "react";
+import {getCars, postCar} from "./services/cars-api";
 
-           </div>
-          );
+export default function App() {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        getCars().then(value => {
+            console.log(value)
+            setCars(value)
+        })
+    }, [])
+
+    function sendCar(e) {
+        e.preventDefault()
+        console.log(e)
+        const model = e.target.name.value;
+        postCar({model})
+    }
+
+    return (
+        <div>
+            <form onClick={sendCar}>
+                <input type="text" name="name"/>
+                <button>send</button>
+            </form>
+
+            {
+                cars.map(value => <div key={value.id}>
+                    Car lot: {value.id}
+                    <br/>Car model: {value.model}
+                </div>)
+            }
+        </div>
+    );
 }
